9652301d9d73003595a9d53885df64a40eeee47a
xinghengwang <xing@moesif.com>
2024-09-19 11:10:24 -0700

    fixed tests  for isBase64

--- tests/testIsBase64.js
+++ tests/testIsBase64.js
@@ -6,33 +6,16 @@ var https = require("https");
 const { describe, it } = require("node:test");
 const util = require("node:util");
 const { ok } = require("assert/strict");
-const { deepEqual, deepStrictEqual } = require("assert");
-const moesif = require("../lib");
 const dataUtils = require('../lib/dataUtils');
 
 
+describe("test isStrBase64", async () => {
 
-describe("testing response for callback-style Lambda", async () => {
-
-  const response = await promiseHandler(event, { ...context });
-  it("should contain 'X-Moesif-Transaction-Id' header", async (t) => {
-    ok(response.headers["X-Moesif-Transaction-Id"]);
+  it("should be valid base 64' header", async (t) => {
+    ok(dataUtils.isStrBase64("eyJmb28iOiJiYXIifQ=="));
   });
 
-  it("should have 'application/json' as the 'Content-Type' header", async (t) => {
-    deepEqual(response.headers["Content-Type"], "application/json");
-  });
-
-  it("should have expected body", async (t) => {
-    deepStrictEqual(
-      response.body,
-      JSON.stringify({
-        key: "hello world",
-      })
-    );
-  });
-
-  it("should have '200 OK' status code", async (t) => {
-    deepStrictEqual(response.statusCode, 200);
+  it("object should not be base 64", async (t) => {
+    ok(!dataUtils.isStrBase64({ foo: "bar"}));
   });
 });
